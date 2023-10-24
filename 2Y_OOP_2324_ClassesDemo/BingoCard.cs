f2fb16dc07d9b28df526c71c5fad9d2d14c7fa1c
lenidar <55443074+lenidar@users.noreply.github.com>
2023-10-24 11:39:40 +0800

    Add project files.

---
+++ 2Y_OOP_2324_ClassesDemo/BingoCard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Y_OOP_2324_ClassesDemo
+{
+    internal class BingoCard
+    {
+        private int[,] _cardNums = new int[5, 5];
+        private Random _rnd = new Random();
+
+        /// <summary>
+        /// This method will generate a bingo card
+        /// </summary>
+        /// <returns>the generated card</returns>
+        public int[,] GenerateCard()
+        {
+            List<int> colNums = new List<int>();
+            int temp = 0;
+
+            InitializeCard();
+
+            for(int y = 0; y < _cardNums.GetLength(1); y++) 
+            {
+                colNums = InitializeList();
+                for(int x = 0; x < _cardNums.GetLength(0); x++)
+                {
+                    temp = _rnd.Next(0, colNums.Count);
+                    _cardNums[x, y] = colNums[temp] + (15 * y);
+                    colNums.RemoveAt(temp);
+                }
+            }
+
+            return _cardNums;
+        }
+
+        private void InitializeCard()
+        {
+            for(int x = 0; x < _cardNums.GetLength(0); x++) 
+            {
+                for(int y = 0;  y < _cardNums.GetLength(1); y++) 
+                {
+                    _cardNums[x, y] = 0;
+                }
+            }
+        }
+
+        private List<int> InitializeList()
+        {
+            List<int> temp = new List<int>();
+            for (int x = 0; x < 15; x++)
+                temp.Add(x + 1);
+            return temp;
+        }
+    }
+}
---
+++ 2Y_OOP_2324_ClassesDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Y_OOP_2324_ClassesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] card = new int[5, 5];
+            BingoCard bc = new BingoCard();
+            card = bc.GenerateCard();
+
+            for(int x = 0; x < card.GetLength(0); x++) 
+            {
+                for(int y = 0; y < card.GetLength(1); y++)
+                {
+                    Console.Write(card[x,y] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+            Console.Clear();
+            int[][,] cards = new int[5][,];
+
+            for(int x = 0; x < cards.Length; x++)
+            {
+                cards[x] = bc.GenerateCard();
+            }
+        }
+    }
+}
